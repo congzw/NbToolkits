17a317f71411a01565b9ca2d8a2c34a3e624687b
congzw <46074987@qq.com>
2017-08-09 12:58:42 +0800

    add NbMd2Html

---
+++ src/NbMd2Html/ArgsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NbMd2Html
+{
+    public class ArgsHelper
+    {
+        public bool IsAppendOnly(string[] args)
+        {
+            var appendOnly = true;
+            if (args.Length > 0)
+            {
+                var append = args[0];
+                if (append.StartsWith("append", StringComparison.OrdinalIgnoreCase))
+                {
+                    var splits = append.Split(new[] { ':', '-', ':', '/' });
+                    if (splits.Length == 1)
+                    {
+                        return true;
+                    }
+                    bool.TryParse(splits[1], out appendOnly);
+                }
+            }
+
+            return appendOnly;
+        }
+    }
+}
---
+++ src/NbMd2Html/ConvertHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace NbMd2Html
+{
+    public class ConvertHelper
+    {
+        public void ProcessFiles(string currentFolder, bool appendOnly)
+        {
+            var mdFiles = Directory.GetFiles(currentFolder, "*.*", SearchOption.TopDirectoryOnly);
+            Console.WriteLine("total find: {0}", mdFiles.Length);
+            Console.WriteLine("=================");
+
+            foreach (var mdFile in mdFiles)
+            {
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(mdFile);
+                var fileName = appendOnly ? mdFile : fileNameWithoutExtension;
+                var htmlFile = fileName + ".html";
+                using (var reader = new StreamReader(mdFile))
+                using (var writer = new StreamWriter(htmlFile))
+                {
+                    CommonMark.CommonMarkConverter.Convert(reader, writer);
+                }
+                Console.WriteLine("convert complete: {0}", htmlFile);
+            }
+        }
+
+    }
+}
