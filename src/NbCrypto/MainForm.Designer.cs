8c3c3b29ba87917d2d940e98bcc0b17b4ab88e70
congzw <2981810300@qq.com>
2018-12-12 21:56:43 +0800

    add encrypt tool

---
+++ src/NbCrypto/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace NbCrypto
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.cbxEncrypt = new System.Windows.Forms.CheckBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtValue.Location = new System.Drawing.Point(12, 12);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(760, 285);
+            this.txtValue.TabIndex = 11;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(697, 331);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "Encrypt";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // cbxEncrypt
+            // 
+            this.cbxEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxEncrypt.AutoSize = true;
+            this.cbxEncrypt.Location = new System.Drawing.Point(676, 336);
+            this.cbxEncrypt.Name = "cbxEncrypt";
+            this.cbxEncrypt.Size = new System.Drawing.Size(15, 14);
+            this.cbxEncrypt.TabIndex = 12;
+            this.cbxEncrypt.UseVisualStyleBackColor = true;
+            this.cbxEncrypt.CheckedChanged += new System.EventHandler(this.cbxEncrypt_CheckedChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(12, 303);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(760, 21);
+            this.txtPassword.TabIndex = 13;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.cbxEncrypt);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnOk);
+            this.Name = "MainForm";
+            this.Text = "Encrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.CheckBox cbxEncrypt;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ src/NbCrypto/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NbCrypto.Helpers;
+
+namespace NbCrypto
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.cbxEncrypt.Checked = true;
+        }
+
+        private void cbxEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            this.btnOk.Text = this.cbxEncrypt.Checked ? "Encrypt" : "Decrypt";
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var password = txtPassword.Text.Trim();
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Password should not null!");
+                return;
+            }
+
+            var raw = this.txtValue.Text;
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                MessageBox.Show("Text should not null!");
+                return;
+            }
+
+            try
+            {
+                var encrypt = this.cbxEncrypt.Checked;
+                if (encrypt)
+                {
+                    var value = AESHelper.EncryptToBase64(raw, password);
+                    this.txtValue.Text = value;
+                }
+                else
+                {
+                    var value = AESHelper.DecryptFromBase64(raw, password);
+                    this.txtValue.Text = value;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid operation");
+            }
+        }
+    }
+}
