2d73fcf27dc5e64920f6970a20410a9187f9df92
congzw <46074987@qq.com>
2021-05-01 08:59:37 +0800

    add template helper

---
+++ src/SwaggerGenerator/Libs/TemplateHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace SwaggerGenerator.Libs
+{
+    public class TemplateHelper
+    {
+        public string Template { get; set; } = string.Empty;
+
+        public string TemplateProcess(SwaggerApiDoc apiDoc)
+        {
+            //todo
+            return Template;
+        }
+    }
+}
