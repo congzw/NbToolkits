b359bdc446f2c39c6fee38cd350e7b2bc8fb3de5
congzw <2981810300@qq.com>
2024-08-27 18:21:51 +0800

    更新NetCore版本

---
+++ src/NbMd2Html.NetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NbMd2Html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentFolder = ".\\";
+            var argsHelper = new ArgsHelper();
+            var isAppendOnly = argsHelper.IsAppendOnly(args);
+            var convertHelper = new ConvertHelper();
+            convertHelper.ProcessFiles(currentFolder, isAppendOnly);
+            WaitForExit();
+        }
+
+        private static void WaitForExit()
+        {
+            Console.WriteLine("process any key exit...");
+            Console.Read();
+        }
+    }
+}
